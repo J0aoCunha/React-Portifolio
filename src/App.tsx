eabac40719e5abb03fd1e410a72ea3cb6c123e5b
J0aoCunha <jvjunger20123@gmail.com>
2024-04-16 01:36:59 -0300

    feat: creating header and add de font style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-[#22212C] p-10 flex flex-row">
+    <div className="w-screen h-screen bg-[#22212C] p-10 flex grid-cols-2 gap-[60px] font-MerriweatherSans">
       <aside className="flex flex-col grid-cols-1 gap-8">
         <Profile
           name="João Victor Da Silva Cunha"
@@ -13,6 +13,20 @@ function App() {
         />
         <Information />
       </aside>
+      <main className="flex-1">
+        <header className="flex items-center p-8 h-[86px] w-full rounded-[20px] bg-[#302F3D] gap-8 justify-between">
+          <h1 className="font-bold text-xl not-italic text-[#837E9F]">
+            My Projects
+          </h1>
+
+          <a
+            href="https://github.com/J0aoCunha?tab=repositories"
+            className="text-[#837E9F] font-normal text-sm leading-normal not-italic"
+          >
+            ver todos
+          </a>
+        </header>
+      </main>
     </div>
   );
 }
