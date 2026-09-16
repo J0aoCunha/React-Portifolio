b9a3e05bb1d542f3284452bc0d5e768efe178fbe
J0aoCunha <jvjunger20123@gmail.com>
2026-09-15 23:31:13 -0300

    corrigindo data de termino de curso

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,16 +1,16 @@
 import { EducationType } from "../@types/EducationType";
 
 export const educations: EducationType[] = [
-  {
-    school: "FAESA",
-    start: "2023",
-    end: "cursando",
-    course: "Bacharelado em Sistemas de Informação",
-  },
-  {
-    school: "Núcleo de Línguas UFES",
-    start: "2016",
-    end: "-",
-    course: "Curso de inglês",
-  },
+	{
+		school: "FAESA",
+		start: "2023",
+		end: "cursando",
+		course: "Bacharelado em Sistemas de Informação",
+	},
+	{
+		school: "Núcleo de Línguas UFES",
+		start: "2016",
+		end: "2020",
+		course: "Curso de inglês",
+	},
 ];
